85ba5d503ce938e47cbd2c9f14ab4ada379095ec
Flosiya <mashameky@gmail.com>
2024-02-24 13:09:15 +0200

    display of the created list

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,14 +3,16 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import ua.edu.lntu.cw3.ui.theme.IPZ_CR_3Theme
-import androidx.compose.material3.Text
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +45,14 @@ fun UserList(users: List<User>) {
 @Composable
 fun UserItem(user: User) {
     Text(text = "User ID: ${user.id}, Name: ${user.name}")
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun UserListPreview() {
+    IPZ_CR_3Theme {
+        UserList(users = generateUserList())
+    }
 }
 
 
