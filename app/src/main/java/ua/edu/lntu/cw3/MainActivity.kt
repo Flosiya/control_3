6f9a823665b57fe99916269899183de8638d0184
Flosiya <mashameky@gmail.com>
2024-02-24 12:55:04 +0200

    vertical list

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,9 +3,12 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import ua.edu.lntu.cw3.ui.theme.IPZ_CR_3Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +29,16 @@ data class User(val id: Int, val name: String)
 fun generateUserList(): List<User> {
     return (1..4).map { User(it, "User $it") }
 }
+
+@Composable
+fun UserList(users: List<User>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(users) { user ->
+            UserItem(user = user)
+        }
+    }
+}
+
 
 
 
