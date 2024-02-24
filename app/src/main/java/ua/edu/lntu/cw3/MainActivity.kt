75950e97e19223cbe866d8176b241f0b2cb78b94
Flosiya <mashameky@gmail.com>
2024-02-24 12:50:44 +0200

    creating a list of users

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -21,4 +21,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class User(val id: Int, val name: String)
+
+fun generateUserList(): List<User> {
+    return (1..3).map { User(it, "User $it") }
+}
+
+
 
